3124bf9cbbc54deba48e8855c08c94a3bed99e30
Anilkumar64 <vennapureddyanil456@gmail.com>
2026-08-22 18:28:02 +0530

    feat: Add getters for course details in CourseCard component

--- force-app/main/default/lwc/courseCard/courseCard.js
+++ force-app/main/default/lwc/courseCard/courseCard.js
@@ -3,6 +3,10 @@ import { LightningElement, api } from 'lwc';
 export default class CourseCard extends LightningElement {
     @api course;
 
+    get courseName() {
+        return this.course?.Name || 'Untitled Course';
+    }
+
     get instructorName() {
         return this.course?.Instructor__r?.Name || 'TBA';
     }
@@ -11,11 +15,24 @@ export default class CourseCard extends LightningElement {
         return this.course?.Instructor__r?.Rating__c || 'N/A';
     }
 
+    get level() {
+        return this.course?.Level__c || '-';
+    }
+
+    get duration() {
+        return this.course?.Duration_Hours__c != null
+            ? this.course.Duration_Hours__c + ' hrs'
+            : '-';
+    }
+
     get priceLabel() {
-        return this.course?.Price__c != null ? '₹' + this.course.Price__c : 'Free';
+        return this.course?.Price__c != null
+            ? '₹' + this.course.Price__c
+            : 'Free';
     }
 
     handleEnroll() {
+        if (!this.course?.Id) return;
         this.dispatchEvent(new CustomEvent('enroll', {
             detail: { courseId: this.course.Id }
         }));
